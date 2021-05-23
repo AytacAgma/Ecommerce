a8a42f4f016867d3cc856ef818bec9cb50221355
Aytac Agma <aytac.agma@netone.net.tr>
2021-05-23 23:18:25 +0300

    Added abstracted Result types with OOP principles and inheritance

--- src/main/java/com/example/northwind/business/abstracts/ProductService.java
+++ src/main/java/com/example/northwind/business/abstracts/ProductService.java
@@ -2,8 +2,11 @@ package com.example.northwind.business.abstracts;
 
 import java.util.List;
 
+import com.example.northwind.core.utilities.results.DataResult;
+import com.example.northwind.core.utilities.results.Result;
 import com.example.northwind.entities.concretes.Product;
 
 public interface ProductService {
-	List<Product> getAll();
+	DataResult<List<Product>> getAll();
+	Result add(Product product);
 }
--- src/main/java/com/example/northwind/business/concretes/ProductManager.java
+++ src/main/java/com/example/northwind/business/concretes/ProductManager.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.northwind.business.abstracts.ProductService;
+import com.example.northwind.core.utilities.results.DataResult;
+import com.example.northwind.core.utilities.results.Result;
+import com.example.northwind.core.utilities.results.SuccessDataResult;
+import com.example.northwind.core.utilities.results.SuccessResult;
 import com.example.northwind.dataAccess.abstracts.ProductDao;
 import com.example.northwind.entities.concretes.Product;
 
@@ -21,8 +25,14 @@ public class ProductManager implements ProductService {
 	}
 
 	@Override
-	public List<Product> getAll() {
-		return this.productDao.findAll();
+	public DataResult<List<Product>> getAll() {
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(),"Data listelendi");
+	}
+
+	@Override
+	public Result add(Product product) {
+		this.productDao.save(product);
+		return new SuccessResult("Ürün eklendi");
 	}
 
 }
